f48f56b94af3f4cac2d1a76edfbf57881b55adb4
Veselin Hitrov <veselin.hitrov@gmail.com>
2015-03-31 01:28:29 +0300

    Clone repository and add hero anim and move.

---
+++ scripts/Player.js
@@ -0,0 +1,43 @@
+var Player=(function(){
+
+    function Player(x,y){
+            this.position = new Vector2(x,y);
+            this.movement={left:false, right:false,up:false, down:false};
+            this.speed=2;
+
+            this.width=32;
+            this.height = 32;
+
+            this.animation=new Animation(this.width, this.height, 0, 0 , 1,
+                                         './images/hero-sprite.png', 22, 4, 4);
+            this.boundingBox = new Rectangle(x, y, this.width, this.height);
+        };
+
+    Player.prototype.update = function() {
+        if(this.movement.right) {
+            this.position.x += this.speed;
+            this.animation.setLimit(5);
+            this.animation.setColumn(3);
+        } else if(this.movement.left) {
+            this.position.x -= this.speed;
+        }
+
+        if(this.movement.up) {
+            this.position.y -= this.speed;
+        } else if(this.movement.down) {
+            this.position.y += this.speed;
+        }
+
+        this.animation.position.set(this.position.x, this.position.y);
+        this.boundingBox.x = this.position.x;
+        this.boundingBox.y = this.position.y;
+
+        this.animation.update();
+    };
+
+    Player.prototype.render=function(ctx){
+        this.animation.draw(ctx)
+    };
+
+    return Player;
+}());
