2024baa7cf825dc1cc2929345f239a18c0df65d7
Ivan Nikolov <ivan.nikolov@ipmediagroup.eu>
2015-04-01 19:31:26 +0300

    Добавена функционалност за камъните

--- scripts/main.js
+++ scripts/main.js
@@ -83,73 +83,72 @@ gem.position.set(rand.x,rand.y);
 
 
     }
+function  renderLevel(lvl) {
 
-    function renderLevel(lvl) {
+    var bgImage = new Image();
+    var borderImage = new Image();
+    var obsImage = new Image();
 
-        var bgImage = new Image();
-        var borderImage = new Image();
-        var obsImage = new Image();
+    switch (lvl) {
 
-        switch (lvl) {
-
-            case 1:
-            {
-                bgImage.src = "images/background1.jpg";
-                obsImage.src = "images/bush.png";
-                borderImage.src = "images/tree.png";
-            }
-                break;
-
-            case 2:
-            {
-                bgImage.src = "images/background2.jpg";
-                obsImage.src = "images/crater.png";
-                borderImage.src = "images/brick.png";
-            }
-                break;
-
-            case 3:
-            {
-                bgImage.src = "images/background3.jpg";
-                obsImage.src = "images/icecrystal.png";
-                borderImage.src = "images/iceblock.png";
-            }
-                break;
+        case 1:
+        {
+            bgImage.src = "images/background1.jpg";
+            obsImage.src = "images/bush.png";
+            borderImage.src = "images/tree.png";
         }
+            break;
 
-        var bgReady = true;
-        var borderReady = true;
-        var obsReady = true;
-
-
-        if (bgReady) {
-            ctx.drawImage(bgImage, 0, 0);
+        case 2:
+        {
+            bgImage.src = "images/background2.jpg";
+            obsImage.src = "images/crater.png";
+            borderImage.src = "images/brick.png";
         }
+            break;
 
-        if (borderReady) {
-            for (var x = 0; x <= col; x += 1) {
-                ctx.drawImage(borderImage, x * 32, 32);
-                ctx.drawImage(borderImage, x * 32, row * 32);
-                levelMap[x][1] = true;
-                levelMap[x][row] = true;
-            }
-            for (var y = 1; y <= row; y += 1) {
-                ctx.drawImage(borderImage, 0, y * 32);
-                ctx.drawImage(borderImage, col * 32, y * 32);
-                levelMap[0][y] = true;
-                levelMap[col][y] = true;
-            }
+        case 3:
+        {
+            bgImage.src = "images/background3.jpg";
+            obsImage.src = "images/icecrystal.png";
+            borderImage.src = "images/iceblock.png";
         }
-        if (obsReady) {
-            for (x = 2; x < col; x += 1) {
-                for (y = 3; y < row; y += 1) {
-                    if (levelMap[x][y]) {
-                        ctx.drawImage(obsImage, x * 32, y * 32);
-                    }
+            break;
+    }
+
+    var bgReady = true;
+    var borderReady = true;
+    var obsReady = true;
+
+
+    if (bgReady) {
+        ctx.drawImage(bgImage, 0, 0);
+    }
+
+    if (borderReady) {
+        for (var x = 0; x <= col; x += 1) {
+            ctx.drawImage(borderImage, x * 32, 32);
+            ctx.drawImage(borderImage, x * 32, row * 32);
+            levelMap[x][1] = true;
+            levelMap[x][row] = true;
+        }
+        for (var y = 1; y <= row; y += 1) {
+            ctx.drawImage(borderImage, 0, y * 32);
+            ctx.drawImage(borderImage, col * 32, y * 32);
+            levelMap[0][y] = true;
+            levelMap[col][y] = true;
+        }
+    }
+    if (obsReady) {
+        for (x = 2; x < col; x += 1) {
+            for (y = 3; y < row; y += 1) {
+                if (levelMap[x][y]) {
+                    ctx.drawImage(obsImage, x * 32, y * 32);
                 }
             }
         }
     }
+}
 
 
     function movePlayer() {
--- scripts/renderBG.js
+++ scripts/renderBG.js
@@ -1,115 +1,71 @@
-// Create the canvas
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 600;
-document.body.appendChild(canvas);
+var renderlevel = function () {
 
-// Background image
-var bgImage;
-bgImage = new Image();
-var bgReady = true;
-bgImage.src = "images/background.png";
+    function  renderLevel(lvl) {
 
-// Border image
+        var bgImage = new Image();
+        var borderImage = new Image();
+        var obsImage = new Image();
 
-var borderImage = new Image();
-var borderReady = true;
+        switch (lvl) {
 
-borderImage.src = "images/tree.png";
+            case 1:
+            {
+                bgImage.src = "images/background1.jpg";
+                obsImage.src = "images/bush.png";
+                borderImage.src = "images/tree.png";
+            }
+                break;
 
-// Obstacle imagee
-var obsImage = new Image();
-var obsReady = true;
-obsImage.src = "images/bush.png";
+            case 2:
+            {
+                bgImage.src = "images/background2.jpg";
+                obsImage.src = "images/crater.png";
+                borderImage.src = "images/brick.png";
+            }
+                break;
 
-var levelMap = [];
-var col = canvas.width/32-1;
-var row = canvas.height/32-1;
-var level = 0.3;
-
-for(var x = 0; x <= col; x++) {
-    levelMap[x] = [];
-    for (var y = 0; y <= row; y++) {
-        levelMap[x][y] = false;
-    }
-}
-
-for (x = 2; x < col; x += 1) {
-    for (y = 3; y < row; y += 1) {
-        levelMap[x][y] = Math.round((Math.random()-level));
-    }
-}
-
-// (levelMap);
-function renderBG() {
-
-    if (bgReady) {
-        ctx.drawImage(bgImage, 0, 0);
-    }
-
-    if (borderReady) {
-        for (var x = 0; x <= col; x += 1) {
-            ctx.drawImage(borderImage, x * 32, 32);
-            ctx.drawImage(borderImage, x * 32, row * 32);
-            levelMap[x][1] = true;
-            levelMap[x][row] = true;
+            case 3:
+            {
+                bgImage.src = "images/background3.jpg";
+                obsImage.src = "images/icecrystal.png";
+                borderImage.src = "images/iceblock.png";
+            }
+                break;
         }
-        for (var y = 1; y <= row; y += 1) {
-            ctx.drawImage(borderImage, 0, y * 32);
-            ctx.drawImage(borderImage, col * 32, y * 32);
-            levelMap[0][y] = true;
-            levelMap[col][y] = true;
+
+        var bgReady = true;
+        var borderReady = true;
+        var obsReady = true;
+
+
+        if (bgReady) {
+            ctx.drawImage(bgImage, 0, 0);
         }
-    }
-    if (obsReady){
-        for (x = 2; x < col; x += 1) {
-            for ( y = 3; y < row; y += 1) {
-                if (levelMap[x][y]) {
-                    ctx.drawImage(obsImage, x * 32, y * 32);
+
+        if (borderReady) {
+            for (var x = 0; x <= col; x += 1) {
+                ctx.drawImage(borderImage, x * 32, 32);
+                ctx.drawImage(borderImage, x * 32, row * 32);
+                levelMap[x][1] = true;
+                levelMap[x][row] = true;
+            }
+            for (var y = 1; y <= row; y += 1) {
+                ctx.drawImage(borderImage, 0, y * 32);
+                ctx.drawImage(borderImage, col * 32, y * 32);
+                levelMap[0][y] = true;
+                levelMap[col][y] = true;
+            }
+        }
+        if (obsReady) {
+            for (x = 2; x < col; x += 1) {
+                for (y = 3; y < row; y += 1) {
+                    if (levelMap[x][y]) {
+                        ctx.drawImage(obsImage, x * 32, y * 32);
+                    }
                 }
             }
         }
     }
+        return renderLevel;
 
-}
-
-
-// Draw everything
-var render = function () {
-
-    ctx.clearRect(0,0,800,600);
-    renderBG();
-
-
-    // Score
-    ctx.fillStyle = "rgb(50, 150, 250)";
-    ctx.font = "24px Helvetica";
-    ctx.textAlign = "left";
-    ctx.textBaseline = "top";
-    ctx.fillText("Gems Collected: " + gemCollected, 32, 0);
-};
-
-// The main game loop
-var main = function () {
-    var now = Date.now();
-    var delta = now - then;
-
-    render();
-
-    then = now;
-
-    // Request to do this again ASAP
-    requestAnimationFrame(main);
-};
-
-// Cross-browser support for requestAnimationFrame
-var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
-
-// Let's play game!
-var then = Date.now();
-render();
-
-
-main();
+}();
